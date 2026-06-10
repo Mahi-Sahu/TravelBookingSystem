af90e98ef9001153ffd984fd905d9f5bd6d9ed46
Mahi Sahu <mahijhs14@gmail.com>
2026-06-10 12:26:10 +0530

    Profile-component added

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { TravelListComponent } from './Components/TravelModule/travel-list-component/travel-list-component';
 import { CustomerDashboardComponent } from './Components/CustomerModule/customer-dashboard-component/customer-dashboard-component';
+import { CustomerProfileComponent } from './Components/CustomerModule/customer-profile-component/customer-profile-component';
 export const routes: Routes = [
   { path: '', component: TravelListComponent },
   { path: 'travel-catalog', component: TravelListComponent },
   {
     path: 'customer/dashboard',
     component: CustomerDashboardComponent,
+  },
+  {
+    path: 'customer/profile',
+    component: CustomerProfileComponent
   },
 ];
