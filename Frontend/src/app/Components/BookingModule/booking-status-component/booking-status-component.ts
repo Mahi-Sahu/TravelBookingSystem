23062e5a0853bb77a858ebf033e5f76cef56d1ac
Mahi Sahu <mahijhs14@gmail.com>
2026-06-11 10:48:12 +0530

    Booking cancellation functionality added

--- Frontend/src/app/Components/BookingModule/booking-status-component/booking-status-component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-booking-status-component',
-  imports: [],
-  templateUrl: './booking-status-component.html',
-  styleUrl: './booking-status-component.css',
-})
-export class BookingStatusComponent {}
